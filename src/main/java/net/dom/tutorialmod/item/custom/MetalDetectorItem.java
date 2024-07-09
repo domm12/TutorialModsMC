cb47463bbb4fe9ae8ce6e5f58591b9d721b845d3
domm12 <dommod01@gmail.com>
2024-07-09 20:27:11 +0300

    tooltip added

--- src/main/java/net/dom/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/dom/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,12 +1,19 @@
 package net.dom.tutorialmod.item.custom;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -41,6 +48,13 @@ public class MetalDetectorItem extends Item{
             player -> player.broadcastBreakEvent(player.getUsedItemHand()));
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents,
+            TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.metal_detector.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
     private void outputValuableCordinates(BlockPos blockPos, Player player, Block block) {
